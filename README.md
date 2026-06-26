7cdd56aa80753fa128a5aea29144818a2f6455c1
prathamMalavi <239937892+prathamMalavi@users.noreply.github.com>
2026-06-26 11:01:37 +0530

    feat: personal configs

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ This repository contains the source code and documentation for the OneCX Local E
 `onecx-local-env` uses Docker and Docker Compose to enable developers to quickly run local instances of OneCX Services.
 
 For more information on `onecx-local-env`, please refer to the [documentation](https://onecx.github.io/docs/documentation/current/onecx-local-env/index.html).
+
+
+```bash
+
+
+```
--- onecx-data/workspace/import-menu.sh
+++ onecx-data/workspace/import-menu.sh
@@ -1,52 +1,52 @@
-#!/usr/bin/env bash
-#
-# Import Workspaces from file for Tenant
-#
-# $1 => tenant
-# $2 => verbose   (true|false)
-#
+# #!/usr/bin/env bash
+# #
+# # Import Workspaces from file for Tenant
+# #
+# # $1 => tenant
+# # $2 => verbose   (true|false)
+# #
 
-set -euo pipefail
+# set -euo pipefail
 
-readonly RED='\033[0;31m'
-readonly GREEN='\033[0;32m'
-readonly CYAN='\033[0;36m'
-readonly YELLOW='\033[0;33m'
-readonly NC='\033[0m' # No Color
-
-
-#################################################################
-# files which have tenant as prefix
-tenant_files=$(ls "${1}"_*_menu.json 2>/dev/null) || true
-SKIP_MSG=""
-if [[ -z "$tenant_files" ]]; then
-  SKIP_MSG=" ==>${RED} skipping${NC}: no tenant files found for menu import"
-fi
-
-printf '%b\n' "$OLE_LINE_PREFIX${CYAN}Importing Workspace Menus via ExIm${NC}\t$SKIP_MSG"
+# readonly RED='\033[0;31m'
+# readonly GREEN='\033[0;32m'
+# readonly CYAN='\033[0;36m'
+# readonly YELLOW='\033[0;33m'
+# readonly NC='\033[0m' # No Color
 
 
-#################################################################
-# operate on found files
-for entry in $tenant_files
-do
-  filename=$(basename "$entry")
-  filename=$(printf '%s' "$filename" | cut -d '.' -f 1)
-  workspace=$(printf '%s' "$filename" | cut -d '_' -f 2)
+# #################################################################
+# # files which have tenant as prefix
+# tenant_files=$(ls "${1}"_*_menu.json 2>/dev/null) || true
+# SKIP_MSG=""
+# if [[ -z "$tenant_files" ]]; then
+#   SKIP_MSG=" ==>${RED} skipping${NC}: no tenant files found for menu import"
+# fi
+
+# printf '%b\n' "$OLE_LINE_PREFIX${CYAN}Importing Workspace Menus via ExIm${NC}\t$SKIP_MSG"
+
+
+# #################################################################
+# # operate on found files
+# for entry in $tenant_files
+# do
+#   filename=$(basename "$entry")
+#   filename=$(printf '%s' "$filename" | cut -d '.' -f 1)
+#   workspace=$(printf '%s' "$filename" | cut -d '_' -f 2)
   
-  url="http://onecx-workspace-svc/exim/v1/workspace/$workspace/menu/import"
-  params="--write-out %{http_code} --silent --output /dev/null -X POST"
-  if [[ "$OLE_SECURITY_AUTH_ENABLED" == "true" ]]; then
-    status_code=$(curl $params -H "$OLE_HEADER_CT_JSON" -H "$OLE_HEADER_AUTH_TOKEN" -H "$OLE_HEADER_APM_TOKEN" -d "@$entry" "$url")
-  else
-    status_code=$(curl $params -H "$OLE_HEADER_CT_JSON" -d "@$entry" "$url")
-  fi
+#   url="http://onecx-workspace-svc/exim/v1/workspace/$workspace/menu/import"
+#   params="--write-out %{http_code} --silent --output /dev/null -X POST"
+#   if [[ "$OLE_SECURITY_AUTH_ENABLED" == "true" ]]; then
+#     status_code=$(curl $params -H "$OLE_HEADER_CT_JSON" -H "$OLE_HEADER_AUTH_TOKEN" -H "$OLE_HEADER_APM_TOKEN" -d "@$entry" "$url")
+#   else
+#     status_code=$(curl $params -H "$OLE_HEADER_CT_JSON" -d "@$entry" "$url")
+#   fi
 
-  if [[ "$status_code" =~ (200|201)$  ]]; then
-    if [[ "${2:-}" == "true" ]]; then
-      printf '    %b\n' "status: ${GREEN}$status_code${NC}, workspace: $workspace"
-    fi
-  else
-    printf '    %b\n' "${RED}status: $status_code, workspace: $workspace ${NC}"
-  fi
-done
+#   if [[ "$status_code" =~ (200|201)$  ]]; then
+#     if [[ "${2:-}" == "true" ]]; then
+#       printf '    %b\n' "status: ${GREEN}$status_code${NC}, workspace: $workspace"
+#     fi
+#   else
+#     printf '    %b\n' "${RED}status: $status_code, workspace: $workspace ${NC}"
+#   fi
+# done
